89b20ce8b9dd18c4333fd17e9d75c21daf081f0e
Ferrer Joshua <ferrer-joshua@nk-create.co.jp>
2025-07-16 17:19:05 +0900

    Added listen and accept, however -22 error on accept

--- server.asm
+++ server.asm
@@ -9,20 +9,24 @@ section .data
   listen_socket dq 0
   client_socket dq 0
 
-  http_ok_response db 'HTTP/1.1 200 OK', 13, 10
-  content_type_header db 'Content-Type: text/html', 13, 10
-  content_length_header db 'Content-Length: ', 0
+  http_ok_response db "HTTP/1.1 200 OK", 13, 10
+  content_type_header db "Content-Type: text/html", 13, 10
+  content_length_header db "Content-Length: ", 0
   ; newline db 13, 10, 13, 10
 
   request_buffer times 1024 db 0
 
   ; Available routes
-  index_route db 'GET / ', 0
-  about_route db 'GET /about ', 0
+  index_route db "GET / ", 0
+  about_route db "GET /about ", 0
 
 section .bss
   sockfd resq 1
   sockaddr resb 16
+
+  client_addr resb 16
+  client_addr_len resq 1
+  clientfd resq 1
 
   content_length_str resb 20
 
@@ -60,7 +64,7 @@ _start:
   mov dword [sockaddr + 4], INADDR_ANY
   mov qword [sockaddr + 8], 0
 
-  ; Bind socket
+  ; Bind port
   mov rax, SYS_BIND
   mov rdi, [sockfd]
   lea rsi, [sockaddr]
@@ -70,6 +74,25 @@ _start:
   test rax, rax
   js fail_port_bind
   
+  ; Listen socket
+  mov rax, SYS_LISTEN
+  mov rdi, [sockfd]
+  mov rsi, BACKLOG
+  syscall
+  test rax, rax
+  js fail_socket_listen
+
+  ; Accept connection
+  mov rax, SYS_ACCEPT
+  mov rdi, [sockfd]
+  lea rsi, [client_addr]
+  lea rdx, [client_addr_len]
+  mov qword [client_addr_len], 16
+  syscall
+  test rax, rax
+  js fail_connection_accept
+
+  mov [clientfd], rax
 
   ; normal exit
   mov rax, 60 ; SYS_EXIT
@@ -81,8 +104,13 @@ fail_socket_create:
   call print_error
 
 fail_port_bind:
-  neg rax
-  
-
   mov rdi, 1 ; port bind error
+  call print_error
+
+fail_socket_listen:
+  mov rdi, 2 ; print listen error
+  call print_error
+
+fail_connection_accept:
+  mov rdi, 3
   call print_error
